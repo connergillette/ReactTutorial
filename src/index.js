d43610fe8937ecbccd098c1be03d2ab5de39b742
Conner Gillette <connerwgillette@gmail.com>
2017-12-04 11:23:44 -0800

    Add changes up to Lecture 12

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create new component - should produce some HTML
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => { // CLASS of component - NOT an instance
+    return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
