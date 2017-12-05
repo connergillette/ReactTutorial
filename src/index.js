760a3e022ad2e43b2e9950e42cfc0ea58c866b54
Conner Gillette <connerwgillette@gmail.com>
2017-12-05 07:39:54 -0800

    Add changes up to Lecture 16

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDIJtl0AX2P81zEpAJcKdO6fKIYqiDEJXY';
+
 // Create new component - should produce some HTML
 const App = () => { // CLASS of component - NOT an instance
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
