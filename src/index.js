b5bca51c661f63cda5e3bc71e7679488544401ed
Conner Gillette <connerwgillette@gmail.com>
2017-12-10 23:46:51 -0800

    Add changes up to Lecture 23

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDIJtl0AX2P81zEpAJcKdO6fKIYqiDEJXY';
 
 // Create new component - should produce some HTML
-const App = () => { // CLASS of component - NOT an instance
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component { // CLASS of component - NOT an instance
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
